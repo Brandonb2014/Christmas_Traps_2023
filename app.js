2deaefea8721b2b6de7b556ad8e637780e2938f2
Brandon <brandon.birschbach@gmail.com>
2023-12-12 21:28:32 -0700

    Adding an api route to process a playerId and sensorId

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+// app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+// app.get('/hi', (req, res) => res.sendFile(path.join(__dirname, 'public', 'hello.html')));
+
+// Express will serve the devault html pages when navigated to them.
+app.use(express.static("public"));
 
 app.get('/api/shortUrls', (req, res) => res.json(urls));
 
@@ -25,33 +29,38 @@ app.get('/:id', (req, res) => {
     return res.sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
-app.post('/shorten', (req, res) => {
-    const { url } = req.query;
-    if (!url) return res.status(400).json({ msg: 'Please provide a URL' });
+app.post('/api', (req, res) => {
+    const { playerId } = req.query;
+    const { sensorId } = req.query;
 
-    try {
-        new URL(url);
-    } catch (err) {
-        return res.status(400).json({ msg: 'Please provide a valid URL' });
+    if (!playerId || !sensorId) return res.status(400).json({ msg: 'Missing playerId or sensorId' });
+
+    switch (playerId) {
+        case '87':
+            console.log('Evelyn just hit sensor', sensorId);
+            break;
+        case '5d':
+            console.log('Wolf just hit sensor', sensorId);
+            break;
+        case '9d':
+            console.log('Carson just hit sensor', sensorId);
+            break;
+        case '7d':
+            console.log('Emerson just hit sensor', sensorId);
+            break;
+        case '3d':
+            console.log('Eye just hit sensor', sensorId);
+            break;
+        default:
+            break;
     }
 
-    // While it is EXTREMELY unlikely that a duplicate ID would be created,
-    // it is still possible so let's make sure it doesn't happen.
-    let newId = '';
-    let existingId = '';
-    do {
-        newId = (Math.random() + 1).toString(36).substring(7);
-        existingId = urls.find(url => url.id === newId);
-    } while (existingId);
-
-    const newShortUrl = {
-        id: newId,
-        url: url
-    };
-
-    urls.push(newShortUrl);
-
-    return res.json({ shortUrl: `${req.protocol}://${req.get('host')}/${newId}`})
+    return res.json({ playerId: `${playerId}`, sensorId: `${sensorId}`})
 });
 
-app.listen(PORT, () => console.log(`Server started on port:${PORT}`));
+// app.listen(PORT, () => console.log(`Server started on port:${PORT}`));
+
+const server = app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
