ccec1b4711075d964949ed61b0270ccd0f1e218c
Brandon <brandon.birschbach@gmail.com>
2023-12-17 00:05:15 -0700

    Adding functionality to record and clear difficulty level

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+// npm run dev to run with nodemon
 import express from 'express';
-import { getPlayers, getPlayer } from './database.js';
+import { getPlayers, getPlayerByName, setPlayerDifficultyLevel, clearPlayerDifficultyLevels, clearPlayerDifficultyLevel } from './database.js';
 
 const app = express();
 app.set("view engine", "ejs");
@@ -9,6 +10,27 @@ const PORT = process.env.PORT || 3000;
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get("/setPlayerDifficultyLevel", async (req, res) => {
+    const { id, difficulty } = req.query;
+
+    if (!id || !difficulty) return res.status(400).json({ msg: 'Missing playerId or difficulty' });
+
+    const result = await setPlayerDifficultyLevel(id, difficulty);
+    res.render("intro.ejs");
+});
+
+app.get("/clearPlayerDifficultyLevels", async (req, res) => {
+    const result = await clearPlayerDifficultyLevels();
+    res.send(result);
+});
+
+app.get("/clearPlayerDifficultyLevel/:name", async (req, res) => {
+    const { name } = req.params;
+    
+    const result = await clearPlayerDifficultyLevel(name);
+    res.send(result);
+});
 
 app.get("/", async (req, res) => {
     const players = await getPlayers();
@@ -20,14 +42,12 @@ app.get("/", async (req, res) => {
 // Express will fall back to the public folder.
 app.use(express.static("public"));
 
-app.get("/player/:id", async (req, res) => {
-    const { id } = req.params;
-    console.log('id:', id);
-    if (!id) return res.status(400).json({ msg: 'Player Id is required' });
+app.get("/player/:name", async (req, res) => {
+    const { name } = req.params;
+    if (!name) return res.status(400).json({ msg: 'Player Name is required' });
 
-    const player = await getPlayer(id);
-    console.log('player:', player);
-    return res.json({ playerId: `${player.id}`, name: `${player.name}`, mission_id: `${player.mission_id}`});
+    const player = await getPlayerByName(name);
+    return res.send(player);
 });
 
 app.get("/players", async (req, res) => {
--- database.js
+++ database.js
@@ -16,7 +16,7 @@ export async function getPlayers() {
     return rows;
 }
 
-export async function getPlayer(id) {
+export async function getPlayerById(id) {
     const [row] = await pool.query(`
         SELECT *
         FROM players
@@ -24,3 +24,39 @@ export async function getPlayer(id) {
     `, [id]);
     return row[0];
 }
+
+export async function getPlayerByName(name) {
+    const [row] = await pool.query(`
+        SELECT *
+        FROM players
+        WHERE name = ?
+    `, [name]);
+    return row[0];
+}
+
+export async function setPlayerDifficultyLevel(id, difficulty) {
+    const [row] = await pool.query(`
+        UPDATE players
+        SET difficulty = ?
+        WHERE id = ?;
+    `, [difficulty, id]);
+    return row;
+}
+
+export async function clearPlayerDifficultyLevels() {
+    const [row] = await pool.query(`
+        UPDATE players
+        SET difficulty = null;
+    `);
+    return row;
+}
+
+export async function clearPlayerDifficultyLevel(name) {
+    const [row] = await pool.query(`
+        UPDATE players
+        SET difficulty = null
+        WHERE name = ?;
+    `, [name]);
+    return await getPlayerByName(name);
+}
+
