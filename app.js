4012addb6dbe99b54a23808ee15d2bf91a6e9d25
Brandon <brandon.birschbach@gmail.com>
2023-12-21 02:21:43 -0700

    Added new dashboard and mission views; Added logic to update the mission page when the player scans their item

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // npm run dev to run with nodemon
 import express from 'express';
-import { getPlayers, getPlayerByName, setPlayerDifficultyLevel, clearPlayerDifficultyLevels, clearPlayerDifficultyLevel } from './database.js';
+import { getPlayers, getPlayerByName, setPlayerDifficultyLevel, clearPlayerDifficultyLevels, clearPlayerDifficultyLevel, getPlayerMissions, saveNewScan, insertPlayerProgress, getPlayerMissionDetails, setPlayerMissionId, getPlayerScans, clearPlayerScans, checkPlayerProgress, updatePlayerProgress, clearPlayerData } from './database.js';
 
 const app = express();
 app.set("view engine", "ejs");
@@ -10,27 +10,6 @@ const PORT = process.env.PORT || 3000;
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.get("/setPlayerDifficultyLevel", async (req, res) => {
-    const { id, difficulty } = req.query;
-
-    if (!id || !difficulty) return res.status(400).json({ msg: 'Missing playerId or difficulty' });
-
-    const result = await setPlayerDifficultyLevel(id, difficulty);
-    res.render("intro.ejs");
-});
-
-app.get("/clearPlayerDifficultyLevels", async (req, res) => {
-    const result = await clearPlayerDifficultyLevels();
-    res.send(result);
-});
-
-app.get("/clearPlayerDifficultyLevel/:name", async (req, res) => {
-    const { name } = req.params;
-    
-    const result = await clearPlayerDifficultyLevel(name);
-    res.send(result);
-});
 
 app.get("/", async (req, res) => {
     const players = await getPlayers();
@@ -55,29 +34,118 @@ app.get("/players", async (req, res) => {
     res.send(players);
 });
 
-app.post('/api', (req, res) => {
+app.get("/setPlayerDifficultyLevel", async (req, res) => {
+    const { id, difficulty } = req.query;
+
+    if (!id || !difficulty) return res.status(400).json({ msg: 'Missing playerId or difficulty' });
+
+    const clearPlayerDataResponse = await clearPlayerData(id);
+
+    const insertPlayerProgressResult = await insertPlayerProgress(id, difficulty);
+
+    const setPlayerDifficultyLevelResult = await setPlayerDifficultyLevel(id, difficulty);
+    res.render("intro.ejs", {
+        id, difficulty,
+    });
+});
+
+app.get("/clearPlayerDifficultyLevels", async (req, res) => {
+    const result = await clearPlayerDifficultyLevels();
+    res.send(result);
+});
+
+app.get("/clearPlayerDifficultyLevel/:name", async (req, res) => {
+    const { name } = req.params;
+    
+    const result = await clearPlayerDifficultyLevel(name);
+    res.send(result);
+});
+
+app.get("/dashboard", async (req, res) => {
+    const { id, difficulty } = req.query;
+
+    // Reset the mission_id of the player back to 0.
+    const setPlayerMissionIdResponse = await setPlayerMissionId(id, 0);
+
+    const missions = await getPlayerMissions(difficulty);
+    res.render("dashboard.ejs", {
+        id, difficulty, missions,
+    });
+});
+
+app.get("/mission", async (req, res) => {
+    const { id, missionId, difficulty } = req.query;
+
+    const setPlayerMissionIdResponse = await setPlayerMissionId(id, missionId);
+
+    const missionDetails = await getPlayerMissionDetails(id, missionId);
+    // console.log('missionDetails:', missionDetails);
+    res.render("mission.ejs", {
+        id, missionId, missionDetails, difficulty,
+    });
+});
+
+app.get("/checkNewScans", async (req, res) => {
+    const { id, missionId } = req.query;
+
+    const getPlayerScansResponse = await getPlayerScans(id);
+    // console.log('getPlayerScansResponse:', getPlayerScansResponse);
+
+    if (!!getPlayerScansResponse && !!getPlayerScansResponse[0]?.sensor_id) {
+        const {sensor_id} = getPlayerScansResponse[0];
+    // console.log('getPlayerScansResponse:', getPlayerScansResponse);
+        if (!!getPlayerScansResponse && !!sensor_id && !!missionId) {
+            const clearPlayerScansResponse = await clearPlayerScans(id);
+
+            // checkPlayerProgress grabs from the player_mission_details table using the player_id, sensor_id, and mission_id
+            const checkPlayerProgressResponse = await checkPlayerProgress(id, sensor_id, missionId);
+            if (!!checkPlayerProgressResponse) {
+                // if data comes back, then we know that the player hit a sensor that is in their mission and we need to update the progress
+                const updatePlayerProgressResponse = await updatePlayerProgress(id, sensor_id, missionId);
+                return res.send(checkPlayerProgressResponse);
+            }
+            return res.status(200).json({ msg: 'No player progress response' });
+        }
+        return res.status(200).json({ msg: 'Missing sensor_id or mission_id' });
+    }
+    return res.status(200).json({ msg: 'No player scans' }); 
+});
+
+app.post('/api', async (req, res) => {
     const { playerId } = req.query;
     const { sensorId } = req.query;
-
+// console.log('api was just hit');
     if (!playerId || !sensorId) return res.status(400).json({ msg: 'Missing playerId or sensorId' });
+
+    const result = await saveNewScan(playerId, sensorId);
+    // res.send(result);
 
 
 
     switch (playerId) {
         case '87':
-            console.log('Evelyn just hit sensor', sensorId);
+            console.log('Evelyn just hit sensor', sensorId, new Date());
             break;
         case '5d':
-            console.log('Wolf just hit sensor', sensorId);
+            console.log('Shelby just hit sensor', sensorId, new Date());
             break;
         case '9d':
-            console.log('Carson just hit sensor', sensorId);
+            console.log('Carson just hit sensor', sensorId, new Date());
             break;
         case '7d':
-            console.log('Emerson just hit sensor', sensorId);
+            console.log('Emerson just hit sensor', sensorId, new Date());
             break;
         case '3d':
-            console.log('Eye just hit sensor', sensorId);
+            console.log('Ollie just hit sensor', sensorId, new Date());
+            break;
+        case '2b':
+            console.log('Grayson just hit sensor', sensorId, new Date());
+            break;
+        case 'a3':
+            console.log('Guest - Pink Emerald just hit sensor', sensorId, new Date());
+            break;
+        case '91':
+            console.log('Guest - Crescent Moon just hit sensor', sensorId, new Date());
             break;
         default:
             break;
--- database.js
+++ database.js
@@ -11,7 +11,7 @@ export async function getPlayers() {
     const [rows] = await pool.query(`
         SELECT *
         FROM players
-        ORDER BY sort_order
+        ORDER BY sort_order;
     `);
     return rows;
 }
@@ -20,7 +20,16 @@ export async function getPlayerById(id) {
     const [row] = await pool.query(`
         SELECT *
         FROM players
-        WHERE id = ?
+        WHERE id = ?;
+    `, [id]);
+    return row[0];
+}
+
+export async function getPlayerSelectedMission(id) {
+    const [row] = await pool.query(`
+        SELECT mission_id
+        FROM players
+        WHERE id = ?;
     `, [id]);
     return row[0];
 }
@@ -29,17 +38,46 @@ export async function getPlayerByName(name) {
     const [row] = await pool.query(`
         SELECT *
         FROM players
-        WHERE name = ?
+        WHERE name = ?;
     `, [name]);
     return row[0];
 }
 
-export async function setPlayerDifficultyLevel(id, difficulty) {
+export async function getPlayerMissions(difficulty) {
+    const [row] = await pool.query(`
+        SELECT *
+        FROM missions
+        WHERE difficulty = ?
+        ORDER BY id;
+    `, [difficulty]);
+    return row;
+}
+
+export async function getMissionDetails(difficulty) {
+    const [row] = await pool.query(`
+        SELECT *
+        FROM mission_details
+        WHERE difficulty = ?
+        ORDER BY id;
+    `, [difficulty]);
+    return row;
+}
+
+export async function setPlayerDifficultyLevel(player_id, difficulty) {
     const [row] = await pool.query(`
         UPDATE players
         SET difficulty = ?
         WHERE id = ?;
-    `, [difficulty, id]);
+    `, [difficulty, player_id]);
+    return row;
+}
+
+export async function setPlayerMissionId(player_id, mission_id) {
+    const [row] = await pool.query(`
+        UPDATE players
+        SET mission_id = ?
+        WHERE id = ?;
+    `, [parseInt(mission_id), player_id]);
     return row;
 }
 
@@ -60,3 +98,90 @@ export async function clearPlayerDifficultyLevel(name) {
     return await getPlayerByName(name);
 }
 
+export async function clearPlayerScans(playerId) {
+    const result = await pool.query(`
+        DELETE FROM new_scans
+        WHERE player_id = ?;
+    `, [playerId]);
+    return result;
+}
+
+export async function clearPlayerData(playerId) {
+    const result = await pool.query(`
+        DELETE FROM player_mission_details
+        WHERE player_id = ?;
+    `, [playerId]);
+    return result;
+}
+
+export async function saveNewScan(playerId, sensorId) {
+    const getPlayerSelectedMissionResult = await getPlayerSelectedMission(playerId);
+    try {
+        if (getPlayerSelectedMissionResult?.mission_id ?? 0 != 0) {
+            const clearPlayerScansResult = await clearPlayerScans(playerId);
+            const [row] = await pool.query(`
+            INSERT INTO new_scans (player_id, sensor_id, mission_id)
+            VALUES (?, ?, ?);
+            `, [playerId, parseInt(sensorId), parseInt(getPlayerSelectedMissionResult.mission_id)]);
+            return row;
+        }
+    } catch (error) {
+        console.log('error saving new scan:', error);
+    }
+}
+
+export async function getPlayerScans(playerId) {
+    const result = await pool.query(`
+        SELECT * FROM new_scans
+        WHERE player_id = ?;
+    `, [playerId]);
+    return result[0];
+}
+
+export async function insertPlayerProgress(playerId, difficulty) {
+    const missionDetails = await getMissionDetails(difficulty);
+
+    if (difficulty == "easy") {
+        const sensor_ids = [2, 9, 7, 4, 1, 3, 6, 8, 5, 10];
+        for (let i = 0; i < missionDetails.length; i++) {
+            const [row] = await pool.query(`
+            INSERT INTO player_mission_details (player_id, sensor_id, mission_id, item, img_url, img_url_complete, display, is_collected)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+            `, [playerId, parseInt(sensor_ids[i]), parseInt(missionDetails[i].mission_id), missionDetails[i].item, missionDetails[i].img_url, missionDetails[i].img_url_complete, missionDetails[i].display, false]);
+        }
+    }
+}
+
+export async function updatePlayerProgress(playerId, sensorId, missionId) {
+    const [row] = await pool.query(`
+        UPDATE player_mission_details
+        SET is_collected = 1
+        WHERE player_id = ?
+        AND sensor_id = ?
+        AND mission_id = ?;
+    `, [playerId, parseInt(sensorId), parseInt(missionId)]);
+    return row;
+}
+
+export async function getPlayerMissionDetails(playerId, missionId) {
+    const [row] = await pool.query(`
+        SELECT *
+        FROM player_mission_details
+        WHERE player_id = ?
+        AND mission_id = ?;
+    `, [playerId, parseInt(missionId)]);
+    return row;
+}
+
+export async function checkPlayerProgress(playerId, sensorId, missionId) {
+    if (!!playerId && !!sensorId && !!missionId) {
+        const [row] = await pool.query(`
+            SELECT *
+            FROM player_mission_details
+            WHERE player_id = ?
+            AND sensor_id = ?
+            AND mission_id = ?;
+        `, [playerId, parseInt(sensorId), parseInt(missionId)]);
+        return row[0];
+    }
+}
